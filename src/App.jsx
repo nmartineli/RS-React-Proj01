ebbad4c1f9c9ca27b6c74fd72c45f35e5f5c40c8
nmartineli <nataliamartineli@gmail.com>
2023-02-15 19:48:18 -0300

    Adicionadas funcionalidades nos comentários e posts

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,45 @@ import { Sidebar } from './components/Sidebar';
 import './global.css';
 import styles from './App.module.css';
 
+const posts = [
+	{
+		id: 1,
+		author: {
+			avatarUrl: 'https://github.com/nmartineli.png',
+			name: 'Natalia Martineli',
+			role: 'Front-End Developer',
+		},
+		content: [
+			{ type: 'paragraph', content: 'Fala galera 👋' },
+			{
+				type: 'paragraph',
+				content:
+					'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+			},
+			{ type: 'link', content: 'jane.design/doctorcare' },
+		],
+		publishedAt: new Date('2023-02-12 20:00:00'),
+	},
+	{
+		id: 2,
+		author: {
+			avatarUrl: 'https://github.com/maykbrito.png',
+			name: 'Mayk Brito',
+			role: 'Educator @Rocketseat',
+		},
+		content: [
+			{ type: 'paragraph', content: 'Fala galera 👋' },
+			{
+				type: 'paragraph',
+				content:
+					'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+			},
+			{ type: 'link', content: 'jane.design/doctorcare' },
+		],
+		publishedAt: new Date('2023-02-11 20:00:00'),
+	},
+];
+
 function App() {
 	return (
 		<div>
@@ -13,7 +52,9 @@ function App() {
 			<div className={styles.wrapper}>
 				<Sidebar />
 				<main>
-					<Post />
+					{posts.map((post) => {
+						return <Post author={post.author} content={post.content} publishedAt={post.publishedAt} />;
+					})}
 				</main>
 			</div>
 		</div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,41 +1,68 @@
+import { useState } from 'react';
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+	const [comments, setComments] = useState(['Post muito bacana, hein?!']);
+
+	const [newCommentText, setNewCommentText] = useState('');
+
+	const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+		locale: ptBR,
+	});
+
+	const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+		locale: ptBR,
+		addSuffix: true,
+	});
+
+	function handleCrateNewComment() {
+		event.preventDefault();
+
+		setComments([...comments, newCommentText]);
+		setNewCommentText('');
+	}
+
+	function handleNewCommentChange() {
+		setNewCommentText(event.target.value);
+	}
 	return (
 		<article className={styles.post}>
 			<header>
 				<div className={styles.author}>
-					<Avatar src="https://github.com/nmartineli.png" />
+					<Avatar src={author.avatarUrl} />
 					<div className={styles.authorInfo}>
-						<strong>Natalia Martineli</strong>
-						<span>Web Developer</span>
+						<strong>{author.name}</strong>
+						<span>{author.role}</span>
 					</div>
 				</div>
-				<time title="14 de outubro às 8h10" dateTime="2022-14-10 08:10:35">
-					Publicado há 1h
+				<time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+					{publishedDateRelativeToNow}
 				</time>
 			</header>
 			<div className={styles.content}>
-				<p>Fala galeraa 👋</p>
-				<p>
-					Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do
-					projeto é DoctorCare 🚀
-				</p>
-				<p>
-					👉<a href="">jane.design/doctorcare</a>
-				</p>
-				<p>
-					<a href="">#novoprojeto</a> <a href="">#nlw </a> <a href="">#rocketseat</a>
-				</p>
+				{content.map((line) => {
+					if (line.type === 'paragraph') {
+						return <p>{line.content}</p>;
+					} else if (line.type === 'link') {
+						return (
+							<p>
+								<a href="#">{line.content}</a>
+							</p>
+						);
+					}
+				})}
 			</div>
 
-			<form className={styles.commentForm}>
+			<form onSubmit={handleCrateNewComment} className={styles.commentForm}>
 				<strong>Deixe seu feedback</strong>
 
-				<textarea placeholder="Deixe um comentário" />
+				<textarea name="comment" placeholder="Deixe um comentário" value={newCommentText} onChange={handleNewCommentChange} />
 
 				<footer>
 					<button type="submit">Publicar</button>
@@ -43,9 +70,9 @@ export function Post() {
 			</form>
 
 			<div className={styles.commentList}>
-				<Comment />
-				<Comment />
-				<Comment />
+				{comments.map((comment) => {
+					return <Comment content={comment} />;
+				})}
 			</div>
 		</article>
 	);
